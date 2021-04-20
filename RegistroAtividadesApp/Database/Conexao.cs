92a7549aeeb8d6f3c4f787e27a9bf30129895afd
joaoteixeirajrr <joaoteixeirajrr@gmail.com>
2021-04-19 22:54:40 -0400

    Tela inicial

---
+++ RegistroAtividadesApp/Database/Conexao.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace RegistroAtividadesApp.Database
+{
+    class Conexao
+    {
+        private static string host = "localhost";
+
+        private static string port = "3308";
+
+        private static string user = "root";
+
+        private static string password = "rootroot";
+
+        private static string dbname = "db_atividades";
+
+        private static MySqlConnection connection;
+
+        private static MySqlCommand command;
+
+        private static MySqlDataAdapter adapter;
+
+        private static DataTable dataTable;
+
+        public Conexao()
+        {
+            try
+            {
+                connection = new MySqlConnection(this.Dns());
+                connection.Open();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public MySqlCommand Query()
+        {
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+
+            }
+            catch (Exception)
+            {
+                connection.Close();
+                throw;
+            }
+
+            return command;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+
+        private string Dns()
+        {
+            return $"server={host};user={user};database={dbname};port={port};password={password}";
+        }
+
+    }
+}
--- RegistroAtividadesApp/MainWindow.xaml.cs
+++ RegistroAtividadesApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RegistroAtividadesApp.Database;
 
 namespace RegistroAtividadesApp
 {
@@ -20,9 +22,101 @@ namespace RegistroAtividadesApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static List<string> listRecurso = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            FillForm();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (FillFormCheck())
+                SaveInfo();
+
+        }
+
+        private void SaveInfo()
+        {
+            try
+            {
+                Conexao conn = new Conexao();
+
+                var query = conn.Query();
+                query.CommandText = "INSERT INTO atividades (servidor, atividade, sistema) " +
+                    "VALUES (@servidor, @atividade, @sistema)";
+                //query.Parameters.AddWithValue("@data", "2021-04-12");
+                query.Parameters.AddWithValue("@servidor", "João Eujácio Teixeira Junior");
+                query.Parameters.AddWithValue("@atividade", txtAtividade.Text);
+                query.Parameters.AddWithValue("@sistema", String.Join(", ", listRecurso));
+                query.ExecuteNonQuery();
+                conn.Close();
+
+                ClearForm();
+
+                MessageBox.Show("Atividade inserida com sucesso!");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private bool FillFormCheck()
+        {
+            //if (listRecurso.Count > 0 && !txtAtividade.Text.Equals("") && !txtAtividade.Text.Equals(null))
+            //    return true;
+            //else
+            //    textBlockInfo.Text = "Preencher os campos antes de salvar...";
+
+            return false;
+        }
+
+        private void FillForm()
+        {
+            string[] recursos = { "Google Meet", "E-mail", "SUAP", "AVA", "SEI" };
+            //cbRecurso.ItemsSource = recursos;
+
+            foreach (var item in recursos)
+            {
+                CheckBox checkbox = new CheckBox();
+                checkbox.Content = item;
+                //checkbox.Width = 100;
+                checkbox.HorizontalAlignment = HorizontalAlignment.Left;
+                checkbox.FontSize = 16;
+                checkbox.Margin = new Thickness(10);
+                //checkbox.Style = this.FindResource("CircleCheckbox") as Style;
+                checkbox.Checked += new RoutedEventHandler(HandleCheck);
+                checkbox.Unchecked += new RoutedEventHandler(HandleUnCheck);
+
+                stackPanelRecurso.Children.Add(checkbox);
+            }
+        }
+
+        private void HandleUnCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox check = sender as CheckBox;
+
+            listRecurso.Remove(check.Content.ToString());
+        }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox check = sender as CheckBox;
+
+            listRecurso.Add(check.Content.ToString());
+        }
+
+        private void ClearForm()
+        {
+            txtAtividade.Text = null;
+        }
+
+        private void GotFocuus(object sender, RoutedEventArgs e)
+        {
+            //textBlockInfo.Text = "";
         }
     }
 }
